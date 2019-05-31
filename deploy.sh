dc7439c3dc9ce4913bf2f7b34d32900d2ebf87e9
johncleveland <jcleve72@gmail.com>
2019-05-31 17:27:04 -0500

    deploy

--- deploy.sh
+++ deploy.sh
@@ -9,16 +9,17 @@ cp -f index.html deploy/
 cp -f sw.js deploy/
 cp -f manifest.json deploy/
 mkdir -p deploy/dist
-cp -fR dist/ deploy/dist/
+#cp -fR dist/ deploy/dist/
+cp -fR dist/ deploy/
 mkdir -p deploy/img
 cp -fR img/ deploy/img/
 
-git checkout gh-pages
-cp -fR deploy/ .
+#git checkout gh-pages
+#cp -fR deploy/ .
 
-rm -rf deploy/
-git add .
-git commit -m "deploy"
-git push origin gh-pages
+#rm -rf deploy/
+#git add .
+#git commit -m "deploy"
+#git push origin gh-pages
 
 #git checkout master
